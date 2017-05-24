53122731cd0eae2821880f59bc779a557dc8188e
zhangyiming <zhangyiming07@163.com>
2017-05-25 00:17:01 +0800

    添加文章逻辑 更新文章逻辑

--- webapp/controllers/blog.py
+++ webapp/controllers/blog.py
@@ -4,10 +4,10 @@ __author__ = "piels"
 import datetime
 from os import path
 from sqlalchemy import func
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, redirect, url_for
 
 from webapp.models import db, Post, Tag, Comment, User, tags
-from webapp.forms import CommentForm
+from webapp.forms import CommentForm, PostForm
 
 
 blog_blueprint = Blueprint(
@@ -57,6 +57,40 @@ def post(post_id):
 
     return render_template('blog/post.html', post=post, tags=tags, comments=comments, recent=recent, top_tags=top_tags,
                            form=form)
+
+
+@blog_blueprint.route('/new', methods=['GET', 'POST'])
+def new_post():
+    form = PostForm()
+
+    if form.validate_on_submit():
+        new_post = Post(form.title.data)
+        new_post.text = form.text.data
+        new_post.publish_date = datetime.datetime.now()
+
+        db.session.add(new_post)
+        db.session.commit()
+
+    return render_template('new.html', form=form)
+
+
+@blog_blueprint.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_post(id):
+    post = Post.query.get_or_404(id)
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.text = form.text.data
+        post.publish_date = datetime.datetime.now()
+
+        db.session.add(post)
+        db.session.commit()
+
+        return redirect(url_for('.post', post_id=post.id))
+
+    form.text.data = post.text
+    return render_template('edit.html', form=form, post=post)
 
 
 @blog_blueprint.route('/tag/<string:tag_name>')
